f36fa1dc4e7ee724f226c9836016ff3dfa4e31ce
GeunChang Ahn <rkaehdaos@gmail.com>
2021-06-21 17:11:54 +0900

    init

---
+++ src/main/java/jpabook/jpashop/JpashopAdvanceApplication.java
@@ -0,0 +1,13 @@
+package jpabook.jpashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpashopAdvanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpashopAdvanceApplication.class, args);
+    }
+
+}
