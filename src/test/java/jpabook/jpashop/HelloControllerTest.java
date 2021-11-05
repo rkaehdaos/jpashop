db330e588a485f23c2ddf98fd82bbd3266204dc7
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2021-11-05 16:44:33 +0900

    HelloControllerTest 테스트 수정

--- src/test/java/jpabook/jpashop/HelloControllerTest.java
+++ src/test/java/jpabook/jpashop/HelloControllerTest.java
@@ -1,17 +1,19 @@
 package jpabook.jpashop;
 
-//import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(controllers = HelloController.class)
@@ -23,7 +25,9 @@ class HelloControllerTest {
     void testHelloController1() throws Exception {
         mockMvc.perform(get("/hello"))
                 .andDo(print())
-                .andExpect(status().isOk());
-
+                .andExpect(status().isOk())
+                .andExpect(model().attribute("data", "hello!!!"))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(content().string(containsString("안뇽")));
     }
 }
