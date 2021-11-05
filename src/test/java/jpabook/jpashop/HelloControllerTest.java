44f746b8c281f02ae4751bfe1fb21727dde3d609
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2021-11-05 15:58:38 +0900

    HelloControllerTest 추가
    MockMvc 주입방법
    - @SpringBootTest + @AutoConfigureMockMvc : 통합테스트시
    - @WebMvcTest : mvc 슬라이스 테스트

---
+++ src/test/java/jpabook/jpashop/HelloControllerTest.java
@@ -0,0 +1,29 @@
+package jpabook.jpashop;
+
+//import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = HelloController.class)
+class HelloControllerTest {
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Test
+    void testHelloController1() throws Exception {
+        mockMvc.perform(get("/hello"))
+                .andDo(print())
+                .andExpect(status().isOk());
+
+    }
+}
