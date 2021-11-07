6a6542662670cba692eaa184d74c3b7a3313cfee
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2021-11-07 18:41:23 +0900

    h2 cmd -> h2.sh

--- run-h2.cmd
+++
@@ -1,1 +0,0 @@
-docker run -d -p 1521:1521 -p 81:81 -v /path/to/local/data_dir:/opt/h2-data -e H2_OPTIONS=-ifNotExists --name=my-h2 rkaehdaos/h2
---
+++ run-h2.sh
@@ -0,0 +1,1 @@
+docker run -d -p 1521:1521 -p 81:81 -v /home/ahn/h2-data:/opt/h2-data -e H2_OPTIONS=-ifNotExists --name=my-h2 rkaehdaos/h2
