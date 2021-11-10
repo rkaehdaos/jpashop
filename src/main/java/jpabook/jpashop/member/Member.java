f36a098f5ac763fe2a638609f2bf08e2dc03841b
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2021-11-10 10:23:46 +0900

    Member 엔티티 : ToString, EqualsAndHashCode 추가
    - 경고
    - Using @EqualsAndHashCode for JPA entities is not recommended. It can cause severe performance and memory consumption issues.

--- src/main/java/jpabook/jpashop/member/Member.java
+++ src/main/java/jpabook/jpashop/member/Member.java
@@ -1,14 +1,17 @@
 package jpabook.jpashop.member;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
-@Getter @Setter
+@Getter @Setter @ToString
+@EqualsAndHashCode(of = "id")
 public class Member {
     @Id @GeneratedValue
     private Long id;
