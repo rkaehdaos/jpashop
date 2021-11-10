6c9de5a05cc40388b231e40279c2a632ab908a66
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2021-11-10 11:20:30 +0900

    MockitoExtension이용한 HelloServiceTest1

--- src/main/java/jpabook/jpashop/hello/HelloService.java
+++ src/main/java/jpabook/jpashop/hello/HelloService.java
@@ -1,8 +1,18 @@
 package jpabook.jpashop.hello;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+
 @Service
+@Transactional
+@RequiredArgsConstructor
 public class HelloService {
+    private final HelloRepository helloRepository;
+
+    public String getHelloMsg(final Long id) {
+        return helloRepository.findById(id).getHelloMsg();
+    }
 
 }
---
+++ src/test/java/jpabook/jpashop/hello/HelloServiceTest.java
@@ -0,0 +1,43 @@
+package jpabook.jpashop.hello;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayName("MockitoExtension 사용 테스트")
+class HelloServiceTest1 {
+    public static final Logger log =  LoggerFactory.getLogger(HelloServiceTest1.class);
+    @InjectMocks
+    HelloService helloService;
+    @Mock
+    HelloRepository helloRepository;
+
+    private Hello hello = Hello.builder()
+            .helloMsg("hello")
+            .helloMsg2(null)
+            .build();
+
+    @Test
+    void mock_test1(){
+        //given
+        when(helloRepository.save(hello)).thenReturn(1L);
+        when(helloRepository.findById(1L)).thenReturn(hello);
+
+        //when
+        Long savedId = helloRepository.save(hello);
+        Hello findedHello = helloRepository.findById(savedId);
+
+        //then
+        assertEquals(hello, findedHello);
+    }
+
+}
