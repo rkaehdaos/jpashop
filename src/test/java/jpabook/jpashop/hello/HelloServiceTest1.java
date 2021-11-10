9ca338d958e48e91f32a5047035af3a23691430b
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2021-11-10 16:33:36 +0900

    관대한 스터빙
    - 사용하지 않는 스텁 감지를 우회

--- src/test/java/jpabook/jpashop/hello/HelloServiceTest1.java
+++ src/test/java/jpabook/jpashop/hello/HelloServiceTest1.java
@@ -9,13 +9,13 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.lenient;
 
 @ExtendWith(MockitoExtension.class)
 @DisplayName("MockitoExtension 사용 테스트")
 class HelloServiceTest1 {
-    public static final Logger log =  LoggerFactory.getLogger(HelloServiceTest1.class);
+    public static final Logger log = LoggerFactory.getLogger(HelloServiceTest1.class);
     @InjectMocks
     HelloService helloService;
     @Mock
@@ -27,15 +27,15 @@ class HelloServiceTest1 {
             .build();
 
     @Test
-    void mock_test1(){
+    void mock_test1() {
         //given
-        when(helloRepository.save(hello)).thenReturn(1L);
-        when(helloRepository.findById(1L)).thenReturn(hello);
+        lenient().when(helloRepository.save(hello)).thenReturn(1L);
+        lenient().when(helloRepository.findById(1L)).thenReturn(hello);
 
         //when
         Long savedId = helloService.setHelloMsg(hello);
         String helloMsg = helloService.getHelloMsg(savedId);
-
+        
         //then
         assertEquals(helloMsg, "hello");
     }
