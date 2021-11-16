71cb6bd09adb197ac067d9414973e7d0ad30ab54
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2021-11-16 11:18:30 +0900

    mac용 스크립트 추가

--- run-h2-macbook.sh
+++ run-h2-macbook.sh
@@ -2,5 +2,6 @@ docker run -d \
        -p 1521:1521 -p 81:81 \
        -v /Users/ahn/h2-data:/opt/h2-data \
        -e H2_OPTIONS=-ifNotExists \
+       --restart always \
        --name=my-h2 \
         rkaehdaos/h2
