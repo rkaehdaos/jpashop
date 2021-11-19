bd719655a9a36047c4fdf960282986deec564b10
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2021-11-19 15:15:27 +0900

    [#6 웹 계층 개발] 에러 컨트롤
    - 에러시 Binding Result에 에러를 넣고 코드가 튕기지 않고 실행됨
    - 에러가 넘겨지면 input의 설정에 의해 css가 바뀜
    - p 설정에 의해 에러메세지 표시
    - 나머지 채운 값도 유지됨

--- src/main/java/jpabook/jpashop/controller/MemberController.java
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -4,8 +4,10 @@ import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -13,6 +15,7 @@ import javax.validation.Valid;
 
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 public class MemberController {
     private final MemberService memberService;
 
@@ -21,8 +24,14 @@ public class MemberController {
         model.addAttribute("memberForm", new memberForm());
         return "members/createMemberForm";
     }
+
     @PostMapping("/members/new")
-    public String create(@Valid memberForm form) {
+    public String create(@Valid memberForm form, BindingResult result) {
+        if (result.hasErrors()) {
+            log.info(result.toString());
+            return "members/createMemberForm";
+        }
+
         Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
         Member member = new Member();
         member.setUsername(form.getName());
