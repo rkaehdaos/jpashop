c0460feb9008050aacf59a8364d180889d024e56
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2021-11-19 16:53:34 +0900

    [#6 웹 계층 개발] modelMapper: Member → membmerForm 타입맵

--- src/main/java/jpabook/jpashop/JpashopAdvanceApplication.java
+++ src/main/java/jpabook/jpashop/JpashopAdvanceApplication.java
@@ -4,9 +4,7 @@ import jpabook.jpashop.controller.memberForm;
 import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Member;
 import org.modelmapper.ModelMapper;
-import org.modelmapper.PropertyMap;
 import org.modelmapper.Provider;
-import org.modelmapper.internal.bytebuddy.build.Plugin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -26,21 +24,31 @@ public class JpashopAdvanceApplication {
         ModelMapper modelMapper = new ModelMapper();
         modelMapper.getConfiguration().setMatchingStrategy(STRICT);
         modelMapper.createTypeMap(memberForm.class, Member.class).setProvider(
-                new Provider<Member>() {
-                    @Override
-                    public Member get(ProvisionRequest<Member> request) {
-                        memberForm form = memberForm.class.cast(request.getSource());
-                        Address address =
-                                new Address(form.getCity(),
-                                        form.getStreet(),
-                                        form.getZipcode());
-                        Member member = new Member();
-                        member.setUsername(form.getName());
-                        member.setAddress(address);
-                        return member;
-                    }
+                request -> {
+                    memberForm form = memberForm.class.cast(request.getSource());
+                    Address address =
+                            new Address(form.getCity(),
+                                    form.getStreet(),
+                                    form.getZipcode());
+                    Member member = new Member();
+                    member.setName(form.getName());
+                    member.setAddress(address);
+                    return member;
                 }
         );
+
+        modelMapper.createTypeMap(Member.class, memberForm.class).setProvider(
+                request -> {
+                    Member member = Member.class.cast(request.getSource());
+                    memberForm memberForm = new memberForm();
+                    memberForm.setName(member.getName());
+                    memberForm.setCity(member.getAddress().getCity());
+                    memberForm.setStreet(member.getAddress().getStreet());
+                    memberForm.setZipcode(member.getAddress().getZipcode());
+                    return memberForm;
+                }
+        );
+
         return modelMapper;
     }
 }
