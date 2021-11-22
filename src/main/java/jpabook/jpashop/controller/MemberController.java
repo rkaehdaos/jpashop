60cd700d86885c54d408b17dcc76808924bb9539
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2021-11-22 17:17:26 +0900

    [#6 웹 계층 개발] 테스트 메모리 DB 다시 적용

--- src/main/java/jpabook/jpashop/controller/MemberController.java
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -10,7 +10,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 
 import javax.validation.Valid;
 import java.util.List;
