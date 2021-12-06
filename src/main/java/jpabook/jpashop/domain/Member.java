8c78d8083f9969f6fd7192f3ce462c0f2b079d48
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2021-12-06 15:26:40 +0900

    [#13 API 개발 고급1] 2번째 에러 발생
    - v1 에러해결처리 방법: 양방향중 하나를 ``@JsonIgnore`
    - 2번째 에러가 발생함

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 
 import javax.persistence.*;
@@ -21,6 +22,7 @@ public class Member {
     @Embedded
     private Address address;
 
+    @JsonIgnore
     @OneToMany(mappedBy = "member")
     private List<Order> orders;
 
