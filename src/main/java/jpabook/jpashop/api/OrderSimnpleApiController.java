47e2ceafa8c66eb212a24d4262cbc6d56a735a8e
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2021-12-06 15:21:43 +0900

    [#13 API 개발 고급1]  에러 -  Could not write JSON

---
+++ src/main/java/jpabook/jpashop/api/OrderSimnpleApiController.java
@@ -0,0 +1,29 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderSearch;
+import jpabook.jpashop.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * xToOne(ManyToOne, OneToOne) 연관 관계 최적화
+ * Order
+ * Order -> Member : ManyToOne
+ * Order -> Delivery : OneToOne
+ */
+@RestController
+@RequiredArgsConstructor
+public class OrderSimnpleApiController {
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/simple-orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        return all;
+    }
+
+}
